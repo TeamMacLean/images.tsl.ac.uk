fb37e510969922d369cd53ea50e94ca04fa2ea40
Martin Page <wookoouk@gmail.com>
2019-05-13 14:50:12 +0100

    show file

--- controllers/files.js
+++ controllers/files.js
@@ -4,6 +4,20 @@ const File = require('../models/file');
 const renderError = require('../lib/renderError');
 module.exports = {
     show: (req, res, next) => {
+        const experimentName = req.params.experiment;
+        const sampleName = req.params.sample;
+        const projectName = req.params.project;
+        const groupName = req.params.group;
+        const captureName = req.params.capture;
+        const fileName = req.params.file;
+        File.find(groupName, projectName, sampleName, experimentName, captureName, fileName)
+            .then(file => {
+                return res.render('files/show', {file})
+            })
+            .catch(err => {
+                return next();
+            })
+
 
     },
     edit: (req, res, next) => {
