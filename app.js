415947009d9c491b64d93fa605c8fea68eba6dc4
Martin Page <wookoouk@gmail.com>
2019-10-21 15:20:16 +0100

    Update app.js

--- app.js
+++ app.js
@@ -37,7 +37,7 @@ app.use(cookieParser());
 
 const uploadApp = express();
 uploadApp.all('*', server.handle.bind(server));
-app.use('/files', uploadApp);
+app.use(config.tusPath, uploadApp);
 
 //app.all([config.tusPath, config.tusPath + '/*', config.tusPath + '/*.*'], tusServer.handle.bind(tusServer));
 
