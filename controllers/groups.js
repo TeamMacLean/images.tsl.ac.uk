2c2dab0a513bc472ccab6791786fa8ff617bc6ad
George Deeks <george@neontribe.co.uk>
2021-06-03 13:19:11 +0100

    debug: groups

--- controllers/groups.js
+++ controllers/groups.js
@@ -9,23 +9,33 @@ module.exports = {
 
                 //TODO if they cannot access the group, mark it as disabled
 
+                console.log('groups', groups);
+                console.log('config', config);
 
                 groups = groups.map(g => {
+
                     const configGroup = config.groups.filter(cf => {
+                        console.log('safename cf.', g.safeName, cf.safeName);
+                        
                         return cf.safeName === g.safeName;
                     });
+                    console.log('configGroup', configGroup);
+                    
                     if (configGroup && configGroup.length && configGroup[0].image) {
+                        console.log('configGroup[0].image', configGroup[0].image)
                         g.image = configGroup[0].image;
                     }
 
-                    if (!Util.canAccessGroup(g.safeName, req)) {
+                    if (!!Util && !!(Util.canAccessGroup) && !Util.canAccessGroup(g.safeName, req)) {
                         g.disabled = true;
                     }
 
-                    if (g.safeName && g.safeName === 'maw'){
+                    if (g.safeName && (g.safeName === 'maw')){
                         g.image = '/img/groups/maw.jpg';
                     }
 
+                    console.log('after', g);
+                    
                     return g;
                 });
                 return res.render('groups/index', {groups});
