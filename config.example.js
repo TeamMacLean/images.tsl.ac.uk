f7f5dcef1657e093aac0e2ae1485d4ef2c51d6ed
George Deeks <georgedeeks@gmail.com>
2024-05-24 10:33:22 +0100

    fix: ports via config

--- config.example.js
+++ config.example.js
@@ -1,33 +1,32 @@
 module.exports = {
-    appName: "Image Data",
-    db: 'imagehog',
-    port: "3000",
-    secret: "keyboard cats!",
-    HPCRoot: "/tsl/data/image", //no trailing slash
-    tmpDir: "/storage/uploads/",
-    postChangesTo:'',
-    tusPath:'/tus',
-    ldap: {
-        url: "ldap://dc.example.org:389",
-        bindDn: "",
-        bindCredentials: "",
-        searchBase: "OU=users,OU=allusers,dc=example,dc=org",
-        searchFilter: "(sAMAccountName={{username}})"
+  appName: "Image Data",
+  rethinkdb: {
+    host: "localhost", // default host
+    port: 28015, // default port
+    db: "imagehog", // default database name
+  },
+  port: "3000",
+  secret: "keyboard cats!",
+  HPCRoot: "/tsl/data/image", //no trailing slash
+  tmpDir: "/storage/uploads/",
+  postChangesTo: "",
+  tusPath: "/tus",
+  ldap: {
+    url: "ldap://dc.example.org:389",
+    bindDn: "",
+    bindCredentials: "",
+    searchBase: "OU=users,OU=allusers,dc=example,dc=org",
+    searchFilter: "(sAMAccountName={{username}})",
+  },
+  uploadTempDir: "/tmp",
+  rootPath: "/my_images", //no trailing slash, this is the local path to the root
+  admins: ["bob", "steve", "chris"],
+  groups: [
+    {
+      name: "Nick's Group",
+      safeName: "ngroup",
+      image: "/img/groups/nick.jpg",
+      adGroups: [""],
     },
-    uploadTempDir: '/tmp',
-    rootPath: '/my_images', //no trailing slash, this is the local path to the root
-    admins: [
-        'bob',
-        'steve',
-        'chris'
-    ],
-    groups: [
-        {
-            name: 'Nick\'s Group',
-            safeName: 'ngroup',
-            image: '/img/groups/nick.jpg',
-            adGroups: ['']
-        }
-
-    ]
+  ],
 };
